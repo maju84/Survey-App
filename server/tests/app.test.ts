af9cd05b33d1b1832dae992f189185906a9ad279
maju84 <max-j@gmx.de>
2022-03-12 22:51:55 +0100

    add 1st real test for GET /api/

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -1,12 +1,13 @@
 import request from "supertest";
 import app from "../src/app";
 
-describe("/test", () => {
+const API = "/api";
+describe(`${API}/`, () => {
 
   test("should respond with a 200 status code", async () => {
-    const response = await request(app).get("/test");
+    const response = await request(app).get(`${API}/`);
     expect(response.statusCode).toBe(200);
-    expect(response.body.smile).toContain(":-)");
+    expect(response.body.surveys).toContain("http");
   })
 
 });
