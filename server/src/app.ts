252dae3481e033cdbd4fe017e39fb3385eb1cc91
maju84 <max-j@gmx.de>
2022-03-13 00:13:17 +0100

    add express config to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { apiRouter, URL_API } from './api/api';
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(URL_API, apiRouter);
