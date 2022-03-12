c95f0209b46c59894e9c1058a2aba2b380e429bc
maju84 <max-j@gmx.de>
2022-03-12 23:27:56 +0100

    refactor tests

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -1,17 +1,18 @@
 import request from "supertest";
 import app from "../src/app";
 
-const API = "/api";
-describe(`${API}/`, () => {
+const URL_API = "/api";
+
+describe(`${URL_API}/`, () => {
 
   test("should respond with a 200 and contain certain string", async () => {
-    const response = await request(app).get(`${API}/`);
+    const response = await request(app).get(`${URL_API}/`);
     expect(response.statusCode).toBe(200);
     expect(response.body.surveys).toContain("http");
   });
 
-  test("should respond with GET,OPTIONS Allowed in header ", async () => {
-    const response = await request(app).options(`${API}/`);
+  test("should respond with GET,OPTIONS 'allow'ed in header", async () => {
+    const response = await request(app).options(`${URL_API}/`);
     expect(response.statusCode).toBe(200);
     expect(response.headers['allow']).toBe('GET,OPTIONS');
   });
