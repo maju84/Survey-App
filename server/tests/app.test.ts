c759053b287500ce5b6571ad0712cb56d9a66f8d
maju84 <max-j@gmx.de>
2022-03-12 22:54:13 +0100

    better test message

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -4,10 +4,10 @@ import app from "../src/app";
 const API = "/api";
 describe(`${API}/`, () => {
 
-  test("should respond with a 200 status code", async () => {
+  test("should respond with a 200 and contain certain string", async () => {
     const response = await request(app).get(`${API}/`);
     expect(response.statusCode).toBe(200);
     expect(response.body.surveys).toContain("http");
-  })
+  });
 
 });
